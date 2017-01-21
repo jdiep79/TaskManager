240bb687839ab668c9e141a30cf4e0556fb8dfbc
Jimmy Diep <jdiep79@gmail.com>
2017-01-21 14:47:43 -0800

    page component to display all task

---
+++ src/app/components/pages/all-task/all-task.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app/components/pages/all-task/all-task.component.html',
+  styleUrls: ['app/components/pages/all-task/all-task.component.css']
+})
+
+export class AllTaskComponent { }

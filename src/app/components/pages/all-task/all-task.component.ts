e847600bf645e4f2726aed85d270912268d85a47
Jimmy Diep <jdiep79@gmail.com>
2017-01-21 15:31:53 -0800

    update links

--- src/app/components/pages/all-task/all-task.component.ts
+++ src/app/components/pages/all-task/all-task.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/components/pages/all-task/all-task.component.html',
-  styleUrls: ['app/components/pages/all-task/all-task.component.css']
+  templateUrl: 'all-task.component.html',
+  styleUrls: ['all-task.component.css']
 })
 
 export class AllTaskComponent { }

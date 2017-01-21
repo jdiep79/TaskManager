29d26afcea0269400a1e1a734c621ec9424533ee
Jimmy Diep <jdiep79@gmail.com>
2017-01-21 14:46:31 -0800

    add formsmodule, httpmodule, and routermodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule
   ],
   declarations: [
     AppComponent

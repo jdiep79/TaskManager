9ba0768a5b3b1bf6005dcbc9230e812c695f2c9d
Jimmy Diep <jdiep79@gmail.com>
2017-01-21 14:48:38 -0800

    page component to display logged in users task

---
+++ src/app/components/pages/user-task/user-task.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import '../../public/css/styles.css';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app/components/pages/user-task/user-task.component.html',
+  styleUrls: ['app/components/pages/user-task/user-task.component.css']
+})
+
+export class AppComponent { }

0662af6761bf60c07c1d9133aa896e4827473cde
Jimmy Diep <jdiep79@gmail.com>
2017-01-21 15:32:15 -0800

    update links

--- src/app/components/pages/update-task/update-task.component.ts
+++ src/app/components/pages/update-task/update-task.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import '../../public/css/styles.css';
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/components/pages/update-task/update-task.component.html',
-  styleUrls: ['app/components/pages/update-task/update-task.component.css']
+  templateUrl: 'update-task.component.html',
+  styleUrls: ['update-task.component.css']
 })
 
 export class UpdateTaskComponent { }

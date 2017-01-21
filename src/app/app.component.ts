f5ec65980e612d3145bb81adc0a91ecf9054286c
Jimmy Diep <jdiep79@gmail.com>
2017-01-21 14:45:42 -0800

    change to absolute path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import '../../public/css/styles.css';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app/app.component.html',
+  styleUrls: ['app/app.component.css']
 })
 
 export class AppComponent { }

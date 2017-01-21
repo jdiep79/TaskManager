8282e42b79755628389b4cd063659ea9ea4be0e8
Jimmy Diep <jdiep79@gmail.com>
2017-01-21 14:48:02 -0800

    page component to update task

---
+++ src/app/components/pages/update-task/update-task.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import '../../public/css/styles.css';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app/components/pages/update-task/update-task.component.html',
+  styleUrls: ['app/components/pages/update-task/update-task.component.css']
+})
+
+export class AppComponent { }

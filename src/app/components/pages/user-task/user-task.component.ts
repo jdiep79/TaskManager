5c15c16e86ec81311c10a8e638f711582a615534
Jimmy Diep <jdiep79@gmail.com>
2017-01-21 14:59:49 -0800

    update component name

--- src/app/components/pages/user-task/user-task.component.ts
+++ src/app/components/pages/user-task/user-task.component.ts
@@ -7,4 +7,4 @@ import '../../public/css/styles.css';
   styleUrls: ['app/components/pages/user-task/user-task.component.css']
 })
 
-export class AppComponent { }
+export class UserTaskComponent { }

8d3f16f8e80c58ac39c8678df79e7e4a0d22c5fd
Jimmy Diep <jdiep79@gmail.com>
2017-01-23 01:33:58 -0800

    add functionality to retrieve user task

--- src/app/components/pages/user-task/user-task.component.ts
+++ src/app/components/pages/user-task/user-task.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskInterface } from '../../../interfaces/task.interface';
+import { TaskService } from '../../../services/task.service';
 
 @Component({
   selector: 'my-app',
@@ -6,4 +8,41 @@ import { Component } from '@angular/core';
   styleUrls: ['user-task.component.css']
 })
 
-export class UserTaskComponent { }
+export class UserTaskComponent implements OnInit { 
+  tasks: TaskInterface[];
+  user: string = 'joshua';
+  errorMessage: string;
+  
+  constructor(private _taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this._taskService.getUsers()
+      .subscribe(users => this._matchUsersToTask(users))
+  }
+
+  private _filterUserTickets(tasks: TaskInterface[]): TaskInterface[] {
+    return tasks.filter(task => {
+      if (!task.users) { return false }
+      return task.users
+        .toLowerCase()
+        .includes(this.user.toLowerCase());
+  
+    });
+  }
+  
+  private _matchUsersToTask(users): TaskInterface[] {
+    this._taskService.getTasks()
+     .subscribe(tasks => {
+       tasks.forEach(task => {
+         users.forEach(user => {
+           if (user.jobs.indexOf(task._id) !== -1) {
+             task.users = !task.users ? user.name : 
+               `${task.users}, ${user.name}`;
+           }
+         })
+       })
+       this.tasks = this._filterUserTickets(tasks);
+     });
+  }
+  
+}

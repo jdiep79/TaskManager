954fd2fd7faf9bf656539753fff7cc5996751779
Jimmy Diep <jdiep79@gmail.com>
2017-01-22 23:39:22 -0800

    import task interface and task service

--- src/app/components/pages/all-task/all-task.component.ts
+++ src/app/components/pages/all-task/all-task.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { TaskInterface } from '../../../interfaces/task.interface';
+import { TaskService } from '../../../services/task.service';
 
 @Component({
   selector: 'my-app',
@@ -6,4 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['all-task.component.css']
 })
 
-export class AllTaskComponent { }
+export class AllTaskComponent implements OnInit { 
+  tasks: TaskInterface[];
+  errorMessage: string;
+  
+  constructor(private _taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this._taskService.getUsers()
+      .subscribe(users => this.matchUsersToTask(users))
+  }
+  
+  private matchUsersToTask(users): void {
+    this._taskService.getTasks()
+     .subscribe(tasks => {
+       tasks.forEach(task => {
+         users.forEach(user => {
+           if (user.jobs.indexOf(task._id) !== -1) {
+             task.users = !task.users ? user.name : 
+               `${task.users}, ${user.name}`;
+           }
+         })
+       })
+       this.tasks = tasks;
+     });
+  }
+  
+}
